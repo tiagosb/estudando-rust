ee226dd92cb20b4f167f1fc309c95397207ff103
tiagosb <tdsb.contato@gmail.com>
2021-01-23 15:22:33 -0300

    cria diretorio para o alo_mundo

---
+++ alo_mundo/target/debug/alo_mundo.d
@@ -0,0 +1,1 @@
+/home/tiago/alo_mundo/alo_mundo/target/debug/alo_mundo: /home/tiago/alo_mundo/alo_mundo/src/main.rs
---
+++ alo_mundo/target/debug/deps/alo_mundo-571d58c505bd42b7.d
@@ -0,0 +1,5 @@
+/home/tiago/alo_mundo/target/debug/deps/alo_mundo-571d58c505bd42b7: src/main.rs
+
+/home/tiago/alo_mundo/target/debug/deps/alo_mundo-571d58c505bd42b7.d: src/main.rs
+
+src/main.rs:
